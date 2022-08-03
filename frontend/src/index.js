0cb3486aca5edb32f21ce0fce155581f3598de76
IronHand88 <107093772+IronHand88@users.noreply.github.com>
2022-08-03 13:33:34 -0500

    first commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import './index.css';
 import App from './App';
